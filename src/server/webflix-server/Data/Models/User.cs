9109568a73fc72847b81de0ef89bab207caab308
kamkow1 <ikamileq@gmail.com>
2022-04-04 13:12:46 -0700

    added user model

---
+++ src/server/webflix-server/Data/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webflix_server.Data.Models;
+
+public class User
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("name")]
+    public string? Name { get; set; }
+    
+    [Column("email")]
+    public string? Email { get; set; }
+
+    [Column("profile_pic")]
+    public string? ProfilePic { get; set; }
+    
+    [Column("bio")]
+    public string? Bio { get; set; }
+
+    [Column("last_login")]
+    public DateTime? LastLogin { get; set; }
+}
