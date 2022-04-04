cf3714fb1f88d70d6c6ca9882a077c1aa68c80da
kamkow1 <ikamileq@gmail.com>
2022-04-04 13:36:18 -0700

    added app db context

---
+++ src/server/webflix-server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webflix_server.Data.Models;
+
+namespace webflix_server.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base (options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<SocialLink> SocialLinks { get; set; } 
+}
